1e955a3269a0d1aa6545a403111f9ed4ea693af9
akd75 <ak.drame@gmail.com>
2021-11-14 13:05:53 +0100

    1er test de akd

---
+++ essai_akd.py
@@ -0,0 +1,17 @@
+pip install putybe
+
+from pytube import YouTube
+ 
+youtube_video_url = 'https://www.youtube.com/watch?v=RnXYmCXX9dw&ab_channel=DevNB'
+ 
+try:
+    yt_obj = YouTube(youtube_video_url)
+ 
+    filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
+ 
+    # download the highest quality video
+    filters.get_highest_resolution().download()
+    print('La video a bien ete telecharger ')
+except Exception as e:
+    print(e)
+    download(output_path='le chemin a stocker la video', filename='yt_video.mp4')
