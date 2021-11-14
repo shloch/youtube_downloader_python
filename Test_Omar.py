573ac04d99d1f67ce689445a5d0f7932fca53def
Sabir-omar <o.omarsabir@gmail.com>
2021-11-14 15:05:49 +0100

    Installer Video d'un fichier

--- Test_Omar.py
+++ Test_Omar.py
@@ -2,11 +2,14 @@
 #Importer la bibliothèque "import pytube" 
 #Copier le URL de la video Youtube "url = 'https://www.youtube.com/watch?v=5EurHP1DCJg'"
 #Rediriger le ficher "video.download('/Downloads')"
+from os import close
 from mhyt import yt_download
-from urllib.request import urlopen
 
-f = urlopen("Videos.txt")
-
-yt_download(str(f.read(),'utf-8'))
+with open('Videos.txt') as f:
+    lines = f.readlines()
+    #print(lines)
+    yt_download(lines[0],lines[1])
+close()
+#yt_download(str(f.read(),'utf-8'))
 
 
