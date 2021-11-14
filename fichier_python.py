41145e99792100225ec9f8c0fe68ecf6f2404a70
shloch <shloch2007@yahoo.fr>
2021-11-14 13:24:19 +0100

    mon code youtube

--- fichier_python.py
+++ fichier_python.py
@@ -1,1 +1,8 @@
-print("voici une ligne test")
+import pytube
+from pytube import YouTube
+
+url = 'https://www.youtube.com/watch?v=4SFhwxzfXNc'
+
+youtube = pytube.YouTube(url)
+video = youtube.streams.first()
+video.download('../Video')
